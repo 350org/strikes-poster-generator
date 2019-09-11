67cf6e144a0a7ec670358bbf29947d0ee6ae286e
Evan Hahn <me@evanhahn.com>
2019-09-11 09:26:03 -0500

    Upgrade to Middleman 4
    This commit upgrades from Middleman 3.3.3 to 4.3.5. That entailed:
    
    1. Upgrading the `middleman` and `middleman-livereload` gems.
    
    2. In the config, converting `set` calls to updates to the `config`
       hash. This wasn't strictly necessary, but [Middleman's docs claim
       that it's a "somewhat newer syntax"][1], so I thought I'd
       future-proof while I was there.
    
    3. Removed Compass. While the ["Upgrading to v4" doc][2] claims that
       Compass was "still bundled by default", I was never able to reproduce
       this. A `git grep -i compass` in the Middleman source gave nothing
       helpful (four comments and one log message; no installed `compass`
       gem or similar). Luckily, we were only using Compass in three
       places—one that was completely unused (`user-select`), one that was
       easy to replicate (the `appearance` mixin), and one that I felt safe
       to replace with the version built into browsers (`transition`).
    
    4. Replaced Sprockets directives with a bunch of included JavaScript
       files. Sprockets seems to have been removed from Middleman v4 and
       relegated to the [`middleman-sprockets` extension][3], but that
       immediately crashed with Sass-related errors (seemingly because it
       was unable to require the [deprecated-since-March `sass` gem][4]). I
       replaced this with a bunch of `<script>` tags. While I'd prefer to
       concatenate these files, that would require setting up some
       additional infrastructure which I felt was out of scope for this
       commit.
    
    Closes [issue #2][5].
    
    [1]: https://middlemanapp.com/advanced/configuration/#changing-settings
    [2]: https://middlemanapp.com/basics/upgrade-v4/
    [3]: https://github.com/middleman/middleman-sprockets
    [4]: https://sass-lang.com/ruby-sass
    [5]: https://github.com/350org/strikes-poster-generator/issues/2

--- Gemfile
+++ Gemfile
@@ -2,10 +2,10 @@
 # the following line to use "https"
 source 'https://rubygems.org'
 
-gem "middleman", "~>3.3.3"
+gem "middleman"
 
 # Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
+gem "middleman-livereload"
 
 # For faster file watcher updates on Windows:
 gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
--- config.rb
+++ config.rb
@@ -1,12 +1,6 @@
 # Reload the browser automatically whenever files change
 activate :livereload
-
-###
-# Compass
-###
-compass_config do |config|
-  config.output_style = :compressed
-end
+config[:livereload_css_target] = nil
 
 ###
 # Helpers
@@ -20,17 +14,17 @@ end
 ###
 # Config
 ###
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-set :url_prefix, '/'
-set :absolute_prefix, 'http://localhost:4567'
+config[:css_dir] = 'stylesheets'
+config[:js_dir] = 'javascripts'
+config[:images_dir] = 'images'
+config[:url_prefix] = '/'
+config[:absolute_prefix] = 'http://localhost:4567'
 
 # Build-specific configuration
 configure :build do
   puts "local build"
-  set :url_prefix, ""
-  set :absolute_prefix, ""
+  config[:url_prefix] = ''
+  config[:absolute_prefix] = ''
   activate :asset_hash
   activate :minify_javascript
   activate :minify_css
